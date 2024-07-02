6f6afa4b7c317d5755c74e599da6e8e765bc049f
Jamie Dumont <jamie@jamies-macbook-pro.local>
2024-07-02 10:42:56 +0100

    Add database, subscribe endpoint and tests.

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,6 @@ pub mod config;
 pub mod routes;
 pub mod startup;
 
-// use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-// use actix_web::dev::Server;
-// use std::net::TcpListener;
-
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use zero2prod::startup::run;
+use zero2prod::config::get_configuration;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
-    run(listener)?.await
+    // Panic if we can't find our config file
+    let config = get_configuration().expect("Failed to read config");
+    let connection_pool = PgPool::connect(
+            &config.database.connection_string()
+        )
+        .await
+        .expect("Failed to connect to Postgres");
+    let address = format!("127.0.0.1:{}", config.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
